b9c9d846df4c11634235d3f69c356a22fa0ac965
xarion <erdicalli@gmail.com>
2017-02-02 17:34:53 +0100

    implemented remaining count

--- filtering.py
+++ filtering.py
@@ -9,6 +9,7 @@ from sqlalchemy import func, or_
 from model import session, Entry
 
 template_string = """
+Remaining: \t%d
 Title: \t\t %s
 Abstract: \t %s
 1: Neurant
@@ -32,15 +33,29 @@ entries = session.query(Entry).filter(or_(
     func.lower(Entry.abstract).like("%neural%"),
     func.lower(Entry.abstract).like("%network%"))
 ).filter(Entry.abstract != None).filter(Entry.read != True).all()
+count = session.query(func.count(Entry)).filter(or_(
+    func.lower(Entry.title).like("%deep%"),
+    func.lower(Entry.title).like("%learning%"),
+    func.lower(Entry.title).like("%neural%"),
+    func.lower(Entry.title).like("%network%"),
+    func.lower(Entry.abstract).like("%deep%"),
+    func.lower(Entry.abstract).like("%learning%"),
+    func.lower(Entry.abstract).like("%neural%"),
+    func.lower(Entry.abstract).like("%network%"))
+).filter(Entry.abstract != None).filter(Entry.read != True).one()
+total= int(count[0])
+print total
+
 
 for entry in entries:
     os.system('clear')
+
     title = entry.title
     abstract = entry.abstract
     for keyword in keywords:
         title = title.replace(keyword, colored(keyword, 'red'))
         abstract = abstract.replace(keyword, colored(keyword, 'red'))
-    print template_string % (title, abstract)
+    print template_string % (total, title, abstract)
     user_input = sys.stdin.readline()
     entry.read = True
     session.add(entry)
@@ -49,3 +64,4 @@ for entry in entries:
         p = subprocess.Popen(['python', 'downloader.py', entry.arxiv_id, user_input])
     if re.match(r'.*q.*', user_input):
         break
+    total -= 1
